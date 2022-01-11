842ab4817e4f73dd62d5506650635d1c096365bf
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-01-11 15:21:18 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+import { createStore } from 'redux'
+
+// reducer
+function reducer(state, action) {
+  console.log('abc')
+}
+
+const store = createStore(reducer);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
