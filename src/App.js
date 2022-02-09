9f883e33900cb655c5f54d5175b32ece35f1e75f
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-02-09 14:53:18 -0600

    connect in Redux

--- src/App.js
+++ src/App.js
@@ -7,49 +7,29 @@ import cartItems from "./cart-items";
 // redux stuff
 
 import { createStore } from 'redux'
-
-import { DECREASE, INCREASE } from "./actions";
-
-// store.getState()
+import { Provider } from "react-redux";
+import reducer from './reducer'
 
 // initial store
 const initialStore = {
-  count: 0,
-  name: "J"
+  cart: cartItems,
+  total: 0,
+  amount: 5,
 }
 
-
-// reducer
-function reducer(state, action) {
-  console.log({ state, action })
-  if (action.type === DECREASE) {
-    return { ...state, count: state.count - 1, name: "B" }
-  }
-  if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 }
-  }
-  return state
-}
 
 
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: DECREASE })
-store.dispatch({ type: INCREASE })
-store.dispatch({ type: INCREASE })
-store.dispatch({ type: INCREASE })
-store.dispatch({ type: INCREASE })
 
-
-console.log(store.getState())
 
 function App() {
   // cart setup
 
   return (
-    <main>
-      <Navbar cart={store.getState()} />
+    <Provider store={store}>
+      <Navbar />
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
